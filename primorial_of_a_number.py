57f55e30de099bebc1c5c2b49cc44cac10c7fbf3
the-carpnter <74491340+the-carpnter@users.noreply.github.com>
2020-11-27 11:37:25 +0530

    Create primorial_of_a_number.py

---
+++ primorial_of_a_number.py
@@ -0,0 +1,22 @@
+def isPrime(n):
+    if n in {0,1}:
+        return False
+    if n == 2:
+        return True
+    if n%2 == 0:
+        return False
+    for i in range(3,int(n**0.5)+1,2):
+        if n%i==0:
+            return False        
+    return True
+
+def num_primorial(n):
+    re = 2
+    count = 1
+    i = 3
+    while True:
+        if count == n:
+            return re
+        if isPrime(i):
+            re *= i
+            count += 1
